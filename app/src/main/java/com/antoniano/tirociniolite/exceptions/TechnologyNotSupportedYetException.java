5f254b203b917e68a2f60bd6c6d376290a56df7e
alessandroquirile <a.quirile@studenti.unina.it>
2021-06-05 20:29:58 +0200

    Minor refactoring

--- app/src/main/java/com/antoniano/tirociniolite/exceptions/TechnologyNotSupportedYetException.java
+++ app/src/main/java/com/antoniano/tirociniolite/exceptions/TechnologyNotSupportedYetException.java
@@ -3,6 +3,6 @@ package com.antoniano.tirociniolite.exceptions;
 public class TechnologyNotSupportedYetException extends RuntimeException {
 
     public TechnologyNotSupportedYetException(String technology) {
-        super(technology + " is not supported yet or invalid");
+        super(technology + " is not supported yet");
     }
 }
